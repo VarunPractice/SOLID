24d0140d2abbf81ed3f26e6ed36aedffd4981d86
Aarush@122015 <you@example.com>
2024-04-19 23:29:57 +0530

    Implementing LSP

---
+++ SOLID/SOLID/LSP/IUnit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.LSP
+{
+    public interface IUnit
+    {
+        void convert(IUnit inputUnit1, double inputUnit1Value, IUnit inputUnit2, double inputUint2Value);
+    }
+}
